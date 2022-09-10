cfb596796311842cfe9d063d622fd1131787e68e
Adio Habib <haadio@student.edu.ng>
2022-09-10 16:19:53 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entrypoint
+ * main -Entrypoint
  *
  * Return:always 0 (success)
  */
 int main (void)
 {
-  puts("programming is like building a multilingual puzzle\n");
+  puts("/"programming is like building a multilingual puzzle");
   return 0;
 }
