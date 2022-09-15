60efa479152949ca5edc8a7d740ee33b8cce8291
Adio Habib <haadio@student.edu.ng>
2022-09-15 04:49:01 -0700

    task 0

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,19 +1,12 @@
-#include <stdio.h>
-#include main.h
+#include "main.h"
 
 /**
  * main - print _putchar
+ *
  * Return: always 0
  */
 int main(void)
 {
-	char a[] = "_putchar"
-	int b = 0;
-do {
-
-_putchar a[b]
-b++;
-} while (b < 8)
-_putchar('\n');
-return (0);
+	_putchar();
+	return (0);
 }
