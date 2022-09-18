7f70fec12003f00baa3e1027a24d4bc6a13e82a8
Adio Habib <haadio@student.edu.ng>
2022-09-18 09:26:41 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * _putchar  - description _putchar function to print the numbers 0 to 9
- *@i: number to print
-
+ * print_numbers  - description to print i
+ *
  * Return: Always 0
  */
 void print_numbers(void)
