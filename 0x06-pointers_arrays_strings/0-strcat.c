48b32e7fbba949d24dd5dd3ae0b5e393c7eab82b
Adio Habib <haadio@student.lautech.edu.ng>
2022-09-25 07:07:13 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+*_strcat - concatenate two strings
+* @dest: destination string
+* @src: source string
+*
+* Return: dest or nothing
+*/
+char *_strcat(char *dest, char *src)
+
+{
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}
