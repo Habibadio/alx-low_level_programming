d940d169cab929783ff0e1872a0a0600fec5efc1
Adio Habib <haadio@student.lautech.edu.ng>
2022-09-27 15:52:33 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - Write a function that fills memory with a constant byte.
+ * @s:starting address of memory to be filled
+ * @b: character to be filled
+ * @n:number of byte to be filled
+ *
+ * Return: return s
+ */
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (0);
+}
