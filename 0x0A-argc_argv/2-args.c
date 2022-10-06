81f3d490d45381a51448ed9acf35bf7fe3843893
Adio Habib <haadio@student.lautech.edu.ng>
2022-10-05 19:48:54 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments
+ * @argc: argument counts
+ * @argv: array containing all strings of command
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+
+{
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("argv[%d] = %s\n", i, argv[i]);
+		}
+	}
+	return (0);
+}
